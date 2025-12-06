0dcf2349618b02daf25330f118243bc7935aecc0
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-12-06 09:49:40 +0100

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,12 +1,14 @@
+// Task 1: Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  location: string;       
+  location: string;
   yearsOfExperience?: number;
   [key: string]: any;
 }
 
+// Example of Teacher
 const teacher3: Teacher = {
   firstName: 'John',
   lastName: 'Doe',
@@ -16,3 +18,35 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
+
+// Task 2: Directors interface
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+// Example of Director
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: true,
+  location: 'London',
+  numberOfReports: 17,
+};
+
+console.log(director1);
+
+// -------------------------------------------
+// Task 3: Printing Teachers
+
+// Interface for the function
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+// Function implementation
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName} ${lastName}`;
+};
+
+// Example usage
+console.log(printTeacher("John", "Doe")); // Output: John Doe
