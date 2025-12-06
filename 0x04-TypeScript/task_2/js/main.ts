a3133c00a06a5c3244f43954ccb48f9b63f15108
Amarire Dev <108834223+Said-Amarire@users.noreply.github.com>
2025-12-06 10:27:59 +0100

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -67,12 +67,6 @@ function executeWork(employee: Director | Teacher): string {
     return employee.workTeacherTasks();
   }
 }
-
-// Example usage
-console.log(executeWork(createEmployee(200)));  // Getting to work
-console.log(executeWork(createEmployee(1000))); // Getting to director tasks
-
-
 // Task 7: String literal types
 
 export type Subjects = 'Math' | 'History';
@@ -80,13 +74,11 @@ export type Subjects = 'Math' | 'History';
 export function teachClass(todayClass: Subjects): string {
   if (todayClass === 'Math') {
     return 'Teaching Math';
-  } else if (todayClass === 'History') {
+  } else {
     return 'Teaching History';
   }
-  return '';
 }
 
 // Example usage
 console.log(teachClass('Math'));    // Teaching Math
 console.log(teachClass('History')); // Teaching History
-
